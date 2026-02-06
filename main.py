99c0ffa113445d79cacaf3a9e9657afe0f9a0df7
Dragoş <edragos33@gmail.com>
2026-02-06 19:15:01 +0100

    functional screen

--- main.py
+++ main.py
@@ -7,14 +7,12 @@ HEIGHT = 1200
 
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 
-def show_start_screen():
-    start = True
-    while start:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                start = False  
-        window.blit(start_image, (0, 0)) 
-        pygame.display.flip()   
+start = True
+while start:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            start = False  
+    pygame.display.flip()   

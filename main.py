26c40517f79ce8ee194db5cb1f2fb172a00b55b6
Dragoş <edragos33@gmail.com>
2026-02-06 19:18:39 +0100

    drawn rectangle

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ HEIGHT = 1200
 
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 
+
 start = True
 while start:
+
+    pygame.draw.rect(screen, (0, 255, 0), (100, 150, 100, 100))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
